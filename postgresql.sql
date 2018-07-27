db45745013f9ef800156c5528a79acc06da0cee3
Mo Yiming <mym@utoronto.edu>
2018-07-27 18:42:20 -0400

    Simplify sql

--- postgresql.sql
+++ postgresql.sql
@@ -19,13 +19,16 @@ SET row_security = off;
 
 SET default_with_oids = false;
 
+CREATE SCHEMA IF NOT EXISTS transit_system;
+SET search_path TO transit_system;
+
 --
 -- TOC entry 200 (class 1259 OID 16403)
 -- Name: card; Type: TABLE; Schema: public; Owner: -
 --
 
-CREATE TABLE public.card (
-    cid integer NOT NULL,
+CREATE TABLE card (
+    cid serial PRIMARY KEY,
     uid integer,
     isactive boolean,
     balance double precision
@@ -33,120 +36,15 @@ CREATE TABLE public.card (
 
 
 --
--- TOC entry 199 (class 1259 OID 16401)
--- Name: card_cid_seq; Type: SEQUENCE; Schema: public; Owner: -
---
-
-CREATE SEQUENCE public.card_cid_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
---
--- TOC entry 3131 (class 0 OID 0)
--- Dependencies: 199
--- Name: card_cid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
-ALTER SEQUENCE public.card_cid_seq OWNED BY public.card.cid;
-
-
---
 -- TOC entry 198 (class 1259 OID 16395)
 -- Name: user; Type: TABLE; Schema: public; Owner: -
 --
 
-CREATE TABLE public."user" (
-    uid integer NOT NULL,
+CREATE TABLE "user" (
+    uid serial PRIMARY KEY,
     name character varying(50),
     email character varying(50),
-    "isAdmin" boolean DEFAULT false,
+    isAdmin boolean DEFAULT false,
     password character varying(100)
 );
-
-
---
--- TOC entry 197 (class 1259 OID 16393)
--- Name: user_uid_seq; Type: SEQUENCE; Schema: public; Owner: -
---
-
-CREATE SEQUENCE public.user_uid_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
---
--- TOC entry 3132 (class 0 OID 0)
--- Dependencies: 197
--- Name: user_uid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
-ALTER SEQUENCE public.user_uid_seq OWNED BY public."user".uid;
-
-
---
--- TOC entry 2996 (class 2604 OID 16406)
--- Name: card cid; Type: DEFAULT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.card ALTER COLUMN cid SET DEFAULT nextval('public.card_cid_seq'::regclass);
-
-
---
--- TOC entry 2994 (class 2604 OID 16398)
--- Name: user uid; Type: DEFAULT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public."user" ALTER COLUMN uid SET DEFAULT nextval('public.user_uid_seq'::regclass);
-
-
---
--- TOC entry 3002 (class 2606 OID 16408)
--- Name: card card_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.card
-    ADD CONSTRAINT card_pkey PRIMARY KEY (cid);
-
-
---
--- TOC entry 2998 (class 2606 OID 16418)
--- Name: user user_email_key; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public."user"
-    ADD CONSTRAINT user_email_key UNIQUE (email);
-
-
---
--- TOC entry 3000 (class 2606 OID 16400)
--- Name: user user_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public."user"
-    ADD CONSTRAINT user_pkey PRIMARY KEY (uid);
-
-
---
--- TOC entry 3003 (class 2606 OID 16409)
--- Name: card card_uid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.card
-    ADD CONSTRAINT card_uid_fkey FOREIGN KEY (uid) REFERENCES public."user"(uid);
-
-
--- Completed on 2018-07-27 18:32:57 EDT
-
---
--- PostgreSQL database dump complete
---
 
